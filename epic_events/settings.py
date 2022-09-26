23d79576a141bc9c28491a9e30c036376bf96e0b
sofiane <abdellaoui.sofiane.esb@gmail.com>
2022-09-26 02:48:27 +0100

    setup postgres database

--- epic_events/settings.py
+++ epic_events/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'epic_events.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'epic_events_dev',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
