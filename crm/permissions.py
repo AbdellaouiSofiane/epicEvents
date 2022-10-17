0a497bd5a91c0585a2783e2eb41c9d05e1a1bb96
sofiane <abdellaoui.sofiane.esb@gmail.com>
2022-10-17 12:51:33 +0200

    update IsInchargeOrReadOnly

--- crm/permissions.py
+++ crm/permissions.py
@@ -1,9 +1,16 @@
+from django.db.models import Q
 from rest_framework import permissions
 
 from .models import Customer, Contract, Event
 
 
 class IsInchargeOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(
+            Q(name='support employees') | Q(name='sales employees')
+        ).exists()
+
     def has_object_permission(self, request, view, obj):
 
         if request.method in permissions.SAFE_METHODS:
